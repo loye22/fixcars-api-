48bb56bb59ececcd0662223662837ff0490fc54e
louye12345 <louye12345@gmail.com>
2025-10-12 13:59:40 +0300

    Added STATICFILES_DIRS and fixed static file configuration for admin CSS

--- carfix/settings.py
+++ carfix/settings.py
@@ -125,6 +125,10 @@ import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'myapp' / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
