116485490820d469a46e3066ee59e104ba5bb124
louye12345 <louye12345@gmail.com>
2025-10-12 11:14:26 +0300

    update the code

--- carfix/settings.py
+++ carfix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h9-d*8nie59rxy6f)ok6bf9cx(+8p(u*23m88_^2b%w+296$!c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1','localhost' , '192.168.1.129', '192.168.1.130', 'fixcars.ro' , 'www.fixcars.ro'  ,'10.0.2.2' ]
+ALLOWED_HOSTS = [ 'fixcars.ro' , 'www.fixcars.ro'  ,'10.0.2.2'  , '192.168.1.129']
 
 
 # Application definition
