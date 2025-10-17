e43140a9128cbc9d1cd70c0d172f1e2a10763285
louye12345 <louye12345@gmail.com>
2025-10-17 18:56:42 +0300

    update the setting to use fixcars emails

--- carfix/settings.py
+++ carfix/settings.py
@@ -167,28 +167,26 @@ SIMPLE_JWT = {
 }
 
 # # Email Configuration fixcars.ro 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'mail.fixcars.ro'
-# EMAIL_PORT = 465
-# # EMAIL_USE_SSL = True
-# # EMAIL_PORT = 587
-# EMAIL_USE_TLS = False
-# EMAIL_USE_SSL = True
-# EMAIL_HOST_USER = 'noreply@fixcars.ro'
-# EMAIL_HOST_PASSWORD = '9nxPfxod.Y[*),b8'  
-# DEFAULT_FROM_EMAIL = 'noreply@fixcars.ro'
-
-# Email Configuration gmail
-# settings.py
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mail.fixcars.ro'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True  # Use TLS (secure connection)
-EMAIL_USE_SSL = False  # Should be False if using TLS
-EMAIL_HOST_USER = 'charlotte.for.it.services@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'ynmj uxbu qgkv hqow'  # The 16-character App Password you generated
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'noreply@fixcars.ro'
+EMAIL_HOST_PASSWORD = '9nxPfxod.Y[*),b8'
+DEFAULT_FROM_EMAIL = 'noreply@fixcars.ro'
+
+
+
+# Email Configuration gmail un comment it in case you want to use gmail to send emails
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True  # Use TLS (secure connection)
+# EMAIL_USE_SSL = False  # Should be False if using TLS
+# EMAIL_HOST_USER = 'charlotte.for.it.services@gmail.com'  # Your Gmail address
+# EMAIL_HOST_PASSWORD = 'ynmj uxbu qgkv hqow'  # The 16-character App Password you generated
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
